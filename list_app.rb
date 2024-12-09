2256d8a926e89570f10a888776f2057f4c430bcc
EC2 Default User <ec2-user@ip-172-31-8-236.ec2.internal>
2024-12-09 19:27:54 +0000

    initial file creation

---
+++ list_app.rb
@@ -0,0 +1,19 @@
+require "sinatra"
+require "sinatra/reloader"
+require "sinatra/content_for"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, SecureRandom.hex(32)
+  set :erb, :escape_html => true
+end
+
+helpers do
+end
+
+get "/" do
+    "test"
+end
+
+
